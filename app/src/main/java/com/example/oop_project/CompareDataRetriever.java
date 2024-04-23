4760aea97c847fd1634cc11add3ef85162cec082
Thet Htar Zin <thethtarzin2001@gmail.com>
2024-04-23 13:39:25 +0200

    Comparing two cities function implemented

---
+++ app/src/main/java/com/example/oop_project/CompareDataRetriever.java
@@ -0,0 +1,256 @@
+package com.example.oop_project;
+
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+public class CompareDataRetriever {
+    static ObjectMapper objectMapper = new ObjectMapper();
+    static HashMap<String, String> cityCodeNames = null;
+
+    public static HashMap<String, String> obtainMunicipalityCodeMap() {
+        if (cityCodeNames == null) {
+            JsonNode areas = readAreaDataFromTheAPIURL(objectMapper);
+            cityCodeNames = createMunicipalityNamesToCodesMap(areas);
+
+        }
+        return cityCodeNames;
+    }
+
+    public ArrayList<WorkplaceSelfSufficiencyData> obtainWorkplaceAndEmploymentData(CompareFragment context, String municipalityName) {
+
+        String code = cityCodeNames.get(municipalityName);
+
+
+        try {
+            JsonNode jsonQuery = objectMapper.readTree(context.getResources().openRawResource(R.raw.workplaceselfsufficiencyquery));
+            ((ObjectNode) jsonQuery.findValue("query").get(1).get("selection")).putArray("values").add(code);
+
+            HttpURLConnection con = connectToAPIAndSendPostRequest(objectMapper, jsonQuery,new URL("https://pxdata.stat.fi:443/PxWeb/api/v1/en/StatFin/tyokay/statfin_tyokay_pxt_125s.px"));
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while((responseLine = br.readLine()) !=null) {
+                    response.append(responseLine.trim());
+                }
+
+                //JsonNode municipalityData = objectMapper.readTree(response.toString());
+
+                JsonNode workAndEmploymentData = objectMapper.readTree(response.toString());
+
+                //WorkplaceSelfSufficiencyData workplaceSelfSufficiencyData = new WorkplaceSelfSufficiencyData();
+                ArrayList<String> years = new ArrayList<>();
+                JsonNode workplaceSelfSufficiency = null;
+
+                for (JsonNode node : workAndEmploymentData.get("dimension").get("Vuosi").get("category").get("label")) {
+                    years.add(node.asText());
+                }
+
+                workplaceSelfSufficiency = workAndEmploymentData.get("value");
+
+                ArrayList<WorkplaceSelfSufficiencyData> workplaceSelfSufficiencyData = new ArrayList<>();
+
+                for (int i = 0; i < workplaceSelfSufficiency.size(); i++) {
+                    double work = workplaceSelfSufficiency.get(i).asDouble();
+                    workplaceSelfSufficiencyData.add(new WorkplaceSelfSufficiencyData(Integer.parseInt(years.get(i)), work));
+                }
+
+                for (WorkplaceSelfSufficiencyData data : workplaceSelfSufficiencyData) {
+                    System.out.println(data.getYear() + ":" + data.getWorkplaceSelfSufficiency() + " ");
+
+                    for (int i = 0; i < data.getWorkplaceSelfSufficiency() / 1000; i++) {
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+
+                return workplaceSelfSufficiencyData;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+    public ArrayList<PopulationData> obtainPopulationData(CompareFragment context, String municipalityName) {
+        String code = cityCodeNames.get(municipalityName);
+        try {
+            JsonNode jsonQuery = objectMapper.readTree(context.getResources().openRawResource(R.raw.populationquery));
+            ((ObjectNode) jsonQuery.findValue("query").get(1).get("selection")).putArray("values").add(code);
+
+            HttpURLConnection con = connectToAPIAndSendPostRequest(objectMapper, jsonQuery, new URL("https://pxdata.stat.fi:443/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px"));
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while((responseLine = br.readLine()) !=null) {
+                    response.append(responseLine.trim());
+                }
+
+                JsonNode compareData = objectMapper.readTree(response.toString());
+
+                ArrayList<String> years = new ArrayList<>();
+                JsonNode comparison = null;
+
+                for (JsonNode node : compareData.get("dimension").get("Vuosi")
+                        .get("category").get("label")) {
+                    years.add(node.asText());
+                }
+
+                comparison = compareData.get("value");
+
+                ArrayList<PopulationData> populationData = new ArrayList<>();
+
+                for (int i = 0; i < comparison.size(); i++) {
+                    Integer population = comparison.get(i).asInt();
+                    populationData.add(new PopulationData(Integer.parseInt(years.get(i)), population));
+
+                }
+
+                System.out.println(municipalityName);
+                System.out.println("==========================");
+
+                for (PopulationData data: populationData) {
+                    System.out.print(data.getYear() + ": " + data.getPopulation()+ " ");
+
+                    for (int i = 0; i< data.getPopulation() / 1000; i++) {
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+
+                return populationData;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public ArrayList<EmploymentData> obtainEmploymentData(CompareFragment context, String municipalityName) {
+
+        String code = cityCodeNames.get(municipalityName);
+
+
+        try {
+            JsonNode jsonQuery = objectMapper.readTree(context.getResources().openRawResource(R.raw.employment));
+            ((ObjectNode) jsonQuery.findValue("query").get(0).get("selection")).putArray("values").add(code);
+
+            HttpURLConnection con = connectToAPIAndSendPostRequest(objectMapper, jsonQuery,new URL("https://pxdata.stat.fi:443/PxWeb/api/v1/en/StatFin/tyokay/statfin_tyokay_pxt_115x.px"));
+
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while((responseLine = br.readLine()) !=null) {
+                    response.append(responseLine.trim());
+                }
+
+                //JsonNode municipalityData = objectMapper.readTree(response.toString());
+
+                JsonNode empData = objectMapper.readTree(response.toString());
+
+                //WorkplaceSelfSufficiencyData workplaceSelfSufficiencyData = new WorkplaceSelfSufficiencyData();
+                ArrayList<String> years = new ArrayList<>();
+                JsonNode employmentRate = null;
+
+                for (JsonNode node : empData.get("dimension").get("Vuosi").get("category").get("label")) {
+                    years.add(node.asText());
+                }
+
+                employmentRate = empData.get("value");
+
+                ArrayList<EmploymentData> employmentData = new ArrayList<>();
+
+                for (int i = 0; i < employmentRate.size(); i++) {
+                    double employment = employmentRate.get(i).asDouble();
+                    employmentData.add(new EmploymentData(Integer.parseInt(years.get(i)), employment));
+                }
+
+                for (EmploymentData data : employmentData) {
+                    System.out.println(data.getYear() + ":" + data.getEmploymentRate() + " ");
+
+                    for (int i = 0; i < data.getEmploymentRate() / 1000; i++) {
+                        System.out.print("*");
+                    }
+                    System.out.println();
+                }
+
+                return employmentData;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    private static HttpURLConnection connectToAPIAndSendPostRequest(ObjectMapper objectMapper, JsonNode jsonQuery, URL url)
+            throws MalformedURLException, IOException, ProtocolException, JsonProcessingException {
+
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json; utf-8");
+        con.setRequestProperty("Accept", "application/json");
+        con.setDoOutput(true);
+
+        try (OutputStream os = con.getOutputStream()) {
+            byte[] input = objectMapper.writeValueAsBytes(jsonQuery);
+            os.write(input, 0, input.length);
+        }
+        return con;
+    }
+
+    private static HashMap<String, String> createMunicipalityNamesToCodesMap(JsonNode areas) {
+        JsonNode codes = null;
+        JsonNode names = null;
+
+        for (JsonNode node: areas.findValue("variables")) {
+            if (node.findValue("text").asText().equals("Area")) {
+                codes = node.findValue("values");
+                names = node.findValue(("valueTexts"));
+            }
+        }
+
+        HashMap<String, String> municipalityNamesToCodesMap = new HashMap<>();
+
+        for (int i =0; i < names.size(); i++) {
+            String name = names.get(i).asText();
+            String code = codes.get(i).asText();
+            municipalityNamesToCodesMap.put(name, code);
+        }
+
+        return municipalityNamesToCodesMap;
+    }
+
+    private static JsonNode readAreaDataFromTheAPIURL(ObjectMapper objectMapper) {
+        JsonNode areas = null;
+        try {
+            areas = objectMapper.readTree(new URL("https://pxdata.stat.fi:443/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px"));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return areas;
+    }
+}
--- app/src/main/java/com/example/oop_project/CompareFragment.java
+++ app/src/main/java/com/example/oop_project/CompareFragment.java
@@ -1,21 +1,153 @@
 package com.example.oop_project;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 
-public class CompareFragment extends Fragment {
+public class CompareFragment extends Fragment  implements View.OnClickListener {
+
+    private EditText editTextCity1;
+    private EditText editTextCity2;
+    Button buttonCompare;
+    private TextView city1Population;
+    private TextView city2Population;
+    private TextView city1Work;
+    private TextView city2Work;
+    private TextView city1EmploymentRate;
+    private TextView city2EmploymentRate;
 
 
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_compare, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_compare, container, false);
+        editTextCity1 = view.findViewById(R.id.search_city1);
+        editTextCity2 = view.findViewById(R.id.search_city2);
+        buttonCompare = view.findViewById(R.id.compareBtn);
+        city1Population = view.findViewById(R.id.c1Population);
+        city2Population = view.findViewById(R.id.c2Population);
+        city1Work = view.findViewById(R.id.c1Work);
+        city2Work = view.findViewById(R.id.c2Work);
+        city1EmploymentRate = view.findViewById(R.id.c1Employ);
+        city2EmploymentRate = view.findViewById(R.id.c2Employ);
+
+        buttonCompare.setOnClickListener(this);
+        return view;
+    }
+
+    public void onClick(View v) {
+        Button clickedButton = (Button) v;
+        if (clickedButton.getId() == R.id.compareBtn) {
+            CompareFragment context = this;
+            CompareDataRetriever compareDataRetriever = new CompareDataRetriever();
+
+            ExecutorService service = Executors.newSingleThreadExecutor();
+            service.execute(new Runnable() {
+                @Override
+                public void run() {
+
+
+                    CompareDataRetriever.obtainMunicipalityCodeMap();
+                    //Population
+                    // Fetch data for city 1
+                    String cityName1 = editTextCity1.getText().toString();
+                    String cityName2 = editTextCity2.getText().toString();
+                    ArrayList<PopulationData> populationDataCity1 = compareDataRetriever.obtainPopulationData(context, editTextCity1.getText().toString());
+                    // Fetch data for city 2
+                    ArrayList<PopulationData> populationDataCity2 = compareDataRetriever.obtainPopulationData(context, editTextCity2.getText().toString());
+                    if (populationDataCity1 == null || populationDataCity2 == null) {
+                        return;
+                    }
+                    //WorkplaceSelfSufficiencyData. Here WPSS stands for workplace self-sufficiency
+                    ArrayList<WorkplaceSelfSufficiencyData> city1WPSS = compareDataRetriever.obtainWorkplaceAndEmploymentData(context, editTextCity1.getText().toString());
+                    ArrayList<WorkplaceSelfSufficiencyData> city2WPSS = compareDataRetriever.obtainWorkplaceAndEmploymentData(context, editTextCity2.getText().toString());
+
+                    if (city1WPSS == null || city2WPSS == null) {
+                        return;
+                    }
+
+                    //Employment
+                    ArrayList<EmploymentData> city1Employment = compareDataRetriever.obtainEmploymentData(context, editTextCity1.getText().toString());
+                    ArrayList<EmploymentData> city2Employment = compareDataRetriever.obtainEmploymentData(context, editTextCity2.getText().toString());
+
+
+
+
+                    // Update UI with comparison results
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+
+                            String M1Population = cityName1+"\n"+"Population\n";
+                            for (PopulationData data : populationDataCity1) {
+                                M1Population = M1Population + data.getYear() + ": " + data.getPopulation() + "\n";
+                            }
+                            city1Population.setText(M1Population);
+                            // Before updating the UI, print the contents of comparisonResults
+
+
+                            String M2Population = cityName2 +"\n"+ "Population\n";
+                            for (PopulationData data : populationDataCity2) {
+                                M2Population = M2Population + data.getYear() + ": " + data.getPopulation() + "\n";
+                            }
+                            city2Population.setText(M2Population);
+
+                            // Before updating the UI, print the contents of comparisonResults
+
+                            //WorkSelfsuficiency of city 1
+                            String M1WorkString = "Workplace self-sufficiency\n";
+
+                            for (WorkplaceSelfSufficiencyData workdata : city1WPSS) {
+                                M1WorkString = M1WorkString + workdata.getYear() + ": " + workdata.getWorkplaceSelfSufficiency() + "\n";
+                            }
+                            city1Work.setText(M1WorkString);
+
+                            // Before updating the UI, print the contents of comparisonResults
+
+                            //WorkSelfsuficiency of city 2
+                            String M2WorkString = "Workplace self-sufficiency\n";
+
+                            for (WorkplaceSelfSufficiencyData workdata : city2WPSS) {
+                                M2WorkString = M2WorkString + workdata.getYear() + ": " + workdata.getWorkplaceSelfSufficiency() + "\n";
+                            }
+                            city2Work.setText(M2WorkString);
+
+
+                            //Employment of city 1
+                            String M1Emp = "Employment Rate\n";
+
+                            for (EmploymentData employmentData : city1Employment) {
+                                M1Emp = M1Emp + employmentData.getYear() + ": " + employmentData.getEmploymentRate() + "\n";
+                            }
+                            city1EmploymentRate.setText(M1Emp);
+
+                            String M2Emp = "Employment Rate\n";
+
+                            for (EmploymentData employmentData : city2Employment) {
+                                M2Emp = M2Emp + employmentData.getYear() + ": " + employmentData.getEmploymentRate() + "\n";
+                            }
+                            city2EmploymentRate.setText(M2Emp);
+
+
+                        }
+                    });
+                }
+            });
+        }
     }
 }
+
